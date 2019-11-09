10316d0c0a54e32530db8ae81013ecf0565fdf70
Pavel Nikoluyk <pashanikoluyk@outlook.com>
2019-11-09 17:05:46 +0200

    second commit

---
+++ PathToMasterCore3/Data/DatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PathToTheMaster.Web.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PathToTheMaster.Web.Data
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+            Database.Migrate();
+        }
+
+        public DbSet<Patient> Patient { get; set; }
+
+        public DbSet<SalePack> SalePack { get; set; }
+
+        public DbSet<Package> Package { get; set; }
+
+        public DbSet<Laboratory> Laboratory { get; set; }
+
+        public DbSet<Group> Group { get; set; }
+
+        public DbSet<City> City { get; set; }
+
+        public DbSet<AnalysisNorm> AnalysisNorm { get; set; }
+
+        public DbSet<Analysis> Analysis { get; set; }
+
+        public DbSet<Addresses> Addresses { get; set; }
+    }
+}
---
+++ PathToMasterCore3/Data/Entity/PackageLaboratory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PathToTheMaster.Web.Data.Entity
+{
+    public class PackageLaboratory
+    {
+        [Key]
+        public int AnalysisId { get; set; }
+
+        [Key]
+        public int LaboratoryId { get; set; }
+
+        [ForeignKey(nameof(AnalysisId))]
+        public virtual Analysis Analysis { get; set; }
+
+        [ForeignKey(nameof(LaboratoryId))]
+        public virtual Laboratory Laboratory { get; set; }
+    }
+}
